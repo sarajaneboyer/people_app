64331a4e54f7ed7d04b82e8a9c5050e32a6a852e
Sara Boyer <sara.jane.boyer@gmail.com>
2020-02-19 15:16:29 -0700

    created Faker in seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+# t.string "first_name"
+# t.integer "age"
+# t.string "phone"
+# t.string "email"
+# t.string "last_name"
+# t.boolean "friend"
+10.times do 
+  contact = Contact.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    age: rand(18...65),
+    hair_color: Faker::Person.hair_color,
+    eye_color: Faker::Person.eye_color,
+    gender: rand(2) == 1 ? F : M,
+    alive: random_boolean = [true,false].sample
+  )
+end
+  # has one address create it
+  # t.string "state"
+  # t.string "city"
+  # t.string "zip"
+  
+  # contact.create_address(
+  #   city: Faker::Address.city, 
+  #   state: Faker::Address.state, 
+  #   zip: Faker::Address.zip_code
+  # )
+  # Has many notes lets make number of notes random for each contact
+  # num_notes = rand(1..3)
+  # messages = [
+  #   Faker::Hipster.sentences(number: 1), 
+  #   Faker::Quote.famous_last_words,
+  #   Faker::ChuckNorris.fact,
+  # ]
+  # num_notes.times do |i|
+  #   contact.notes.create(body: messages[i])
+  # end
